cd6b6a4812f1d5ec9de145306c7f68c44ec341ec
gaurav.prasad <gaurav.prasad@phonepe.com>
2024-08-28 21:05:46 +0530

    Adding zk jdk and ubuntu docker

---
+++ ubuntu/script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+name="myubuntu"
+
+case ${1} in
+	"build" ) 
+		echo "Building...."
+		docker build . -t ${name}
+	;;
+	"run" )
+		echo "Running..."
+		docker run -d --rm --platform linux/x86_64 --name ${name} -it ${name}
+	;;
+
+	"exec" )
+		echo "Executing..."
+		id=$(docker ps -q -a --no-trunc -f name=$name )
+		docker exec -it ${id} bash
+	;;
+
+	"rm" )
+		id=$(docker ps -q -a --no-trunc -f name=$name )
+		docker rm ${id}
+	;;
+	"stop" )
+		echo "Stopping..."
+		id=$(docker ps -q -a --no-trunc -f name=$name )
+		docker stop ${id}
+	;;
+
+esac
+
