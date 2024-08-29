013e9ec0eb0936d719c5f901021e2c259c426068
gaurav.prasad <gaurav.prasad@phonepe.com>
2024-08-29 10:38:30 +0530

    Adding setup script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Repository for list of docker images
+
+First run the setup script to download required binaries
+```commandline
+bash ./setup.sh
+```
+
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://downloads.apache.org/zookeeper/zookeeper-3.9.2/apache-zookeeper-3.9.2-bin.tar.gz -P ./binary/
+wget https://download.oracle.com/java/17/archive/jdk-17_linux-x64_bin.tar.gz -P ./binary/
